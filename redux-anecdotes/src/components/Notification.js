b486c57d27c4f39da6410cf3853e76efe17e4a60
yc <10258156+yaochie@users.noreply.github.com>
2020-04-27 23:36:08 -0400

    exercise 6.21

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Notification = () => {
-  const notification = useSelector(state => state.message)
+  const notification = useSelector(state =>
+    state.message === null ? null : state.message.message
+  )
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
     display: notification === null ? 'none': '',
   }
+
   return (
     <div style={style}>
       {notification}
--- redux-anecdotes/src/reducers/messageReducer.js
+++ redux-anecdotes/src/reducers/messageReducer.js
@@ -1,7 +1,11 @@
 const messageReducer = (state = null, action) => {
   switch(action.type) {
-    case 'SET_MESSAGE':
+    case 'SET_MESSAGE': {
+      if (state !== null && state.timeoutId !== null) {
+        clearTimeout(state.timeoutId)
+      }
       return action.data
+    }
     default:
       return state
   }
@@ -9,14 +13,18 @@ const messageReducer = (state = null, action) => {
 
 export const setMessage = (message, timeout) => {
   return async dispatch => {
-    dispatch({
-      type: 'SET_MESSAGE',
-      data: message
-    })
-    setTimeout(() => dispatch({
+    const timeoutId = setTimeout(() => dispatch({
       type: 'SET_MESSAGE',
       data: null
     }), timeout * 1000)
+
+    dispatch({
+      type: 'SET_MESSAGE',
+      data: {
+        message,
+        timeoutId
+      }
+    })
   }
 }
 
